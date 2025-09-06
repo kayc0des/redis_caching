8dc263f015323352c19adb095e67e6a4b31dae1e
kayc0des <k.boafo@alustudent.com>
2025-09-06 18:41:24 +0200

    chore: requirements.txt

---
+++ cache/main.py
@@ -0,0 +1,13 @@
+"""
+There's no goal. Just messing around with Redis.
+"""
+
+import redis
+from config import Settings
+
+if __name__ == '__main__':
+    redis_sess = redis.Redis(host=Settings.REDIS_HOST, port=Settings.REDIS_PORT, db=Settings.REDIS_DB, decode_responses=True)
+    
+    redis_sess.set('username', 'kayc0des')
+    print(redis_sess.get('username'))
+    

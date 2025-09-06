f6c935f3f818570460815b6f3fddec399b007e8a
kayc0des <k.boafo@alustudent.com>
2025-09-06 19:09:04 +0200

    ref: adding expiy or 'time-to-live'

--- cache/main.py
+++ cache/main.py
@@ -8,6 +8,6 @@ from config import Settings
 if __name__ == '__main__':
     redis_sess = redis.Redis(host=Settings.REDIS_HOST, port=Settings.REDIS_PORT, db=Settings.REDIS_DB, decode_responses=True)
     
-    redis_sess.set('username', 'kayc0des')
+    redis_sess.set('username', 'kayc0des', ex=20)
     print(redis_sess.get('username'))
     
